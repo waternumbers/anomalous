7b1ae6c077587e8cdc5b6778c38515a4f0abb33e
Paul Smith <paul@waternumbers.co.uk>
2023-07-07 01:41:40 +0100

    CROPS: Added additional catch to stop inf loop + fixed output

--- R/crops.R
+++ R/crops.R
@@ -7,12 +7,12 @@
 #' @param min_length minimum number of values in a collective anomaly
 #' @param prune logical, should pruning be used
 #' @param verbose logical, print out progress
-#'
+#' @param maxIter maximum number of algorithm evaluations to perform
 #' @return something...
 #'
 #' @details This will only work for cost functions where the beta is additive!!!
 #' @export
-crops <- function(betaMin,betaMax,fCost,alg=pelt,betaP=Inf,min_length=2,prune=TRUE,verbose=FALSE,...){
+crops <- function(betaMin,betaMax,fCost,alg=pelt,betaP=Inf,min_length=2,prune=TRUE,verbose=FALSE,maxIter=100,...){
     ##browser()
     betaRec <- c(betaMin,betaMax)
     qRec <- rep(NA,2)
@@ -29,13 +29,15 @@ crops <- function(betaMin,betaMax,fCost,alg=pelt,betaP=Inf,min_length=2,prune=TR
     intervals <- list( c(1,2) )
 
     if( all(mRec==mRec[1]) ){ stop("Starting conditions produce the same solution") }
-        
-    while(length(intervals)>0){
+
+    cnt <- 0
+    while(length(intervals)>0 & cnt < maxIter){
         ii <- intervals[[1]][1]
         jj <- intervals[[1]][2]
         if( mRec[ii] > mRec[jj]+1 ){
             bint <- (qRec[jj] - qRec[ii]) / (mRec[ii] - mRec[jj])
             if(!is.finite(bint)){ browser() }
+            if( bint %in% betaRec ){ break } ## seems to be needed but not sure why..
             tmp <- alg(partition(bint,betaP,min_length),fCost)
             kk <- nrow(collective_anomalies(tmp))
             if(kk!=mRec[jj]){
@@ -43,21 +45,24 @@ crops <- function(betaMin,betaMax,fCost,alg=pelt,betaP=Inf,min_length=2,prune=TR
                 mRec <- c(mRec,kk)
                 qRec <- c(qRec,tmp$cost - kk*bint)
                 
-                if(paste(kk) %in% names(outRec)){
+                if(!(paste(kk) %in% names(outRec))){
                     outRec[[paste(kk)]] <- tmp
                 }
                 intervals <- c(intervals,
                                list(c(ii,length(betaRec))),
                                list(c(length(betaRec),jj)))
             }
+            cnt <- cnt + 1
         }
         intervals <- intervals[-1]
+        ##print(intervals)
     }
+    if(cnt==maxIter){ warning("Maximum numbers of iterations reached") }
     return(outRec)
     ## list(betaRec=betaRec,
-    ##             qRec=qRec,
-    ##             mRec=mRec,
-    ##             outRec=outRec))
+    ##              qRec=qRec,
+    ##              mRec=mRec,
+    ##              outRec=outRec)
 }
 
     
