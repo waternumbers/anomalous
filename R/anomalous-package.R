6d444cfa94121f82911a7ebb653d6bf06d85b9fe
Paul Smith <paul@waternumbers.co.uk>
2023-10-17 16:18:01 +0100

    Crops (#3)
    * initial file generation
    
    * initial implimentation of 1D crops
    
    * CROPS: Added additional catch to stop inf loop + fixed output
    
    * minor workings
    
    * Altered CROPS return is single mRec

--- R/anomalous-package.R
+++ R/anomalous-package.R
@@ -5,6 +5,7 @@
 #' @name anomalous-package
 #' @docType package
 #' @importFrom R6 R6Class
+#' @importFrom stats dnorm
 # #' @importFrom methods new
 # #' @importFrom Rcpp loadModule
 # #' @useDynLib anomalous, .registration=TRUE
